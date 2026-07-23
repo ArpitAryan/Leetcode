7b1d4d68ec4f83b30831ccd649d769186d2e7822
Arpit kumar <arpitkumar.work@gmail.com>
2026-07-23 12:44:29 +0530

    Time: 17 ms (5.77%), Space: 46.9 MB (5.2%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,41 +1,30 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-
-        TreeMap<String, Integer> map = new TreeMap<>();
-
-        // Count frequency
-        for (int i = 0; i < words.length; i++) {
-            if (map.containsKey(words[i])) {
-                int val = map.get(words[i]);
-                map.put(words[i], val + 1);
-            } else {
-                map.put(words[i], 1);
+        TreeMap<String,Integer>map=new TreeMap<>();
+        for(int i =0;i<words.length;i++){
+            if(map.containsKey(words[i])){
+                int val=map.get(words[i]);
+                map.put(words[i],val+1);
+            }else{
+                map.put(words[i],1);
             }
         }
-
-        List<String> ans = new ArrayList<>();
-
-        while (k > 0) {
-
-            String maxWord = "";
-            int maxFreq = -1;
-
-            for (String word : map.keySet()) {
-
-                if (map.get(word) > maxFreq) {
-                    maxFreq = map.get(word);
-                    maxWord = word;
-                }
+        List<String>ans=new ArrayList<>();
+        while(k>0){
+            String maxword="";
+            int maxfreq=-1;
+            for(String word:map.keySet()){
+                if(map.get(word)>maxfreq){
+                    maxword=word;
+                    maxfreq=map.get(word);
+                }else if(map.get(word) == maxfreq && word.compareTo(maxword) < 0){
+                    maxword = word;
+}
             }
-
-            ans.add(maxWord);
-
-            
-            map.remove(maxWord);
-
+            ans.add(maxword);
+            map.remove(maxword);
             k--;
         }
-
         return ans;
     }
 }
