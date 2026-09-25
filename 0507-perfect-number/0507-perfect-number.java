cd36770df40b1e0123458aa20b7c025b8d9034a1
Arpit kumar <arpitkumar.work@gmail.com>
2026-09-25 19:54:19 +0530

    Time: 1 ms (97.32%), Space: 42.5 MB (10.14%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-
-        if(num <= 1)return false;
         
+        if(num <= 1)return false;
+
         int sum = 1;
 
-        for(int i = 2; i < num; i++){
+        for(int i = 2; i * i <= num; i++){
             if(num % i == 0){
                 sum += i;
+
+                if(i != num/i){
+                    sum += num/i;
+                }
             }
         }
 
-        if(sum == num){
-            return true;
-        } else {
-            return false;
-        }
+        return sum == num;
     }
 }
